2a1348256094a47835cc4df2bcc03188fb9baa3d
AzureAD\KrzysztofKobiałka <krzysztof.kobialka@wealtharc.com>
2024-11-05 20:28:08 +0100

    Day 2 of practice

--- api_practice.py
+++ api_practice.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+from sqlalchemy import create_engine
+
 
 url = "https://v3.football.api-sports.io/leagues"
 
@@ -10,13 +12,21 @@ headers = {
 }
 
 leagues = requests.request("GET", url, headers=headers, data=payload).json()['response']
-
-polish_leagues = []
+country_leagues = []
 
 for league in leagues:
-    if league['country']['name'] == 'Poland':
-        polish_leagues.append(league['league'])
+    if league['country']['name'] != 'World':
+        league_record = league['league']
+        del league_record['logo']
+        league_record['country'] = league['country']['name']
+        country_leagues.append(league_record)
 
-df = pd.DataFrame(polish_leagues)
-df.to_csv('polish_football_leagues', sep=',', index=False, header=True)
+df = pd.DataFrame(country_leagues)
+# print(type(df))
 
+
+
+df.to_csv('country_leagues', sep=',', index=False, header=True)
+
+engine = create_engine('postgresql://xxx:xxx@localhost:5432/xxx')
+df.to_sql('country_football_leagues', engine, if_exists='replace')
